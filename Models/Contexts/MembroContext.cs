9128578ea981aae7ca8549b2f611d7ec293694c7
Saulo Regis <l4910@liga.org.br>
2023-07-27 08:14:35 -0300

    Adição do context e adição do dbcontext

--- Models/Contexts/MembroContext.cs
+++ Models/Contexts/MembroContext.cs
@@ -1,10 +1,29 @@
 using ipcsmembros.Models.Entities;
+using ipcsmembros.Models.EntityConfigurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace ipcsmembros.Contexts
 {
+
     public class MembroContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+
+        public MembroContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public DbSet<Membro> Membros => Set<Membro>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("ipcsmembros"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new MembroConfiguration());
+        }
     }
 }
