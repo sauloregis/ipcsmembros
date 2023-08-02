94b4729e614ab31ae6cfc6c999bb71a8dce767f0
Saulo Regis <l4910@liga.org.br>
2023-08-02 08:13:22 -0300

    Mudanças de layout

---
+++ Controllers/MembrosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ipcsmembros.Controllers
+{
+    public class MembrosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
