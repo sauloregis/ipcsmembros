1acd0e6d32f238f98fc426a189383912b4939038
Saulo Regis <l4910@liga.org.br>
2023-08-03 16:50:04 -0300

    Ajuste da Listagem de membros

--- Controllers/MembrosController.cs
+++ Controllers/MembrosController.cs
@@ -14,16 +14,17 @@ namespace ipcsmembros.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string filtro)
         {
-            var membros = _context.Membros.Select(x => new ListarMembroViewModel
-            {
-                Id = x.Id,
-                Nome = x.Nome,
-                Email = x.Email,
-                DataNascimento = x.DataNascimento
-            });
-
+            var membros = _context.Membros.Where(x => x.Nome.Contains(filtro) || x.Email.Contains(filtro))
+                                          .Select(x => new ListarMembroViewModel
+                                          {
+                                              Id = x.Id,
+                                              Nome = x.Nome,
+                                              Email = x.Email,
+                                              DataNascimento = x.DataNascimento
+                                          });
+            ViewBag.Filtro = filtro;
             return View(membros);
         }
     }
